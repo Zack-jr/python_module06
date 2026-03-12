6d9e66e00a10ab2d9cd789799e7ce27766950450
Zakaria Labib-Girard <girard.zack04@gmail.com>
2026-03-12 12:51:52 +0100

    typo fix and flake8

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,7 +1,7 @@
 from .elements import create_fire, create_water
 
 __version__ = "1.0.0"
-__author__ = "Master Pythonicius"
+__author__ = "Master Pythonicus"
 
-
+__all__ = [create_fire, create_water]
 # "header" file
